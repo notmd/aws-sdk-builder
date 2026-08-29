431597672e8b678481e47852a467f8bb99aea5a3
notmd <notmd1811@gmail.com>
2026-08-29 13:17:55 +0700

    Refresh s3 generated documentation

--- crates/aws_sdk_s3_modular/src/client.rs
+++ crates/aws_sdk_s3_modular/src/client.rs
@@ -23,7 +23,7 @@ pub(crate) struct Handle {
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let config = aws_config::load_from_env().await;
-/// let client = aws_sdk_s3::Client::new(&config);
+/// let client = aws_sdk_s3_modular::Client::new(&config);
 /// # }
 /// ```
 ///
@@ -35,7 +35,7 @@ pub(crate) struct Handle {
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let sdk_config = ::aws_config::load_from_env().await;
-/// let config = aws_sdk_s3::config::Builder::from(&sdk_config)
+/// let config = aws_sdk_s3_modular::config::Builder::from(&sdk_config)
 /// # /*
 ///     .some_service_specific_setting("value")
 /// # */
@@ -219,8 +219,8 @@ mod create_session;
 /// operation call. For example, this can be used to add an additional HTTP header:
 ///
 /// ```ignore
-/// # async fn wrapper() -> ::std::result::Result<(), aws_sdk_s3::Error> {
-/// # let client: aws_sdk_s3::Client = unimplemented!();
+/// # async fn wrapper() -> ::std::result::Result<(), aws_sdk_s3_modular::Error> {
+/// # let client: aws_sdk_s3_modular::Client = unimplemented!();
 /// use ::http_1x::header::{HeaderName, HeaderValue};
 ///
 /// let result = client.abort_multipart_upload()
--- crates/aws_sdk_s3_modular/src/lib.rs
+++ crates/aws_sdk_s3_modular/src/lib.rs
@@ -52,12 +52,12 @@
 //! Then in code, a client can be created with the following:
 //!
 //! ```rust,no_run
-//! use aws_sdk_s3 as s3;
+//! use aws_sdk_s3_modular as s3;
 //!
 //! #[::tokio::main]
 //! async fn main() -> Result<(), s3::Error> {
 //!     let config = aws_config::load_from_env().await;
-//!     let client = aws_sdk_s3::Client::new(&config);
+//!     let client = aws_sdk_s3_modular::Client::new(&config);
 //!
 //!     // ... make some calls with the client
 //!
@@ -65,7 +65,7 @@
 //! }
 //! ```
 //!
-//! See the [client documentation](https://docs.rs/aws-sdk-s3/latest/aws_sdk_s3/client/struct.Client.html)
+//! See the [client documentation](https://docs.rs/aws-sdk-s3/latest/aws_sdk_s3_modular/client/struct.Client.html)
 //! for information on what calls can be made, and the inputs and outputs for each of those calls.
 //!
 //! ## Using the SDK
@@ -126,7 +126,7 @@ pub use config::Config;
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let config = aws_config::load_from_env().await;
-/// let client = aws_sdk_s3::Client::new(&config);
+/// let client = aws_sdk_s3_modular::Client::new(&config);
 /// # }
 /// ```
 ///
@@ -138,7 +138,7 @@ pub use config::Config;
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let sdk_config = ::aws_config::load_from_env().await;
-/// let config = aws_sdk_s3::config::Builder::from(&sdk_config)
+/// let config = aws_sdk_s3_modular::config::Builder::from(&sdk_config)
 /// # /*
 ///     .some_service_specific_setting("value")
 /// # */
