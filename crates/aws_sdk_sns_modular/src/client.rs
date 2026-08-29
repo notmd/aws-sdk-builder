33d960f764173d6956c8a4d3c4aea27672d18fc1
notmd <notmd1811@gmail.com>
2026-08-29 13:17:55 +0700

    Refresh sns generated documentation

--- crates/aws_sdk_sns_modular/src/client.rs
+++ crates/aws_sdk_sns_modular/src/client.rs
@@ -23,7 +23,7 @@ pub(crate) struct Handle {
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let config = aws_config::load_from_env().await;
-/// let client = aws_sdk_sns::Client::new(&config);
+/// let client = aws_sdk_sns_modular::Client::new(&config);
 /// # }
 /// ```
 ///
@@ -35,7 +35,7 @@ pub(crate) struct Handle {
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let sdk_config = ::aws_config::load_from_env().await;
-/// let config = aws_sdk_sns::config::Builder::from(&sdk_config)
+/// let config = aws_sdk_sns_modular::config::Builder::from(&sdk_config)
 /// # /*
 ///     .some_service_specific_setting("value")
 /// # */
@@ -164,8 +164,8 @@ mod create_topic;
 /// operation call. For example, this can be used to add an additional HTTP header:
 ///
 /// ```ignore
-/// # async fn wrapper() -> ::std::result::Result<(), aws_sdk_sns::Error> {
-/// # let client: aws_sdk_sns::Client = unimplemented!();
+/// # async fn wrapper() -> ::std::result::Result<(), aws_sdk_sns_modular::Error> {
+/// # let client: aws_sdk_sns_modular::Client = unimplemented!();
 /// use ::http_1x::header::{HeaderName, HeaderValue};
 ///
 /// let result = client.add_permission()
--- crates/aws_sdk_sns_modular/src/lib.rs
+++ crates/aws_sdk_sns_modular/src/lib.rs
@@ -46,12 +46,12 @@
 //! Then in code, a client can be created with the following:
 //!
 //! ```rust,no_run
-//! use aws_sdk_sns as sns;
+//! use aws_sdk_sns_modular as sns;
 //!
 //! #[::tokio::main]
 //! async fn main() -> Result<(), sns::Error> {
 //!     let config = aws_config::load_from_env().await;
-//!     let client = aws_sdk_sns::Client::new(&config);
+//!     let client = aws_sdk_sns_modular::Client::new(&config);
 //!
 //!     // ... make some calls with the client
 //!
@@ -59,7 +59,7 @@
 //! }
 //! ```
 //!
-//! See the [client documentation](https://docs.rs/aws-sdk-sns/latest/aws_sdk_sns/client/struct.Client.html)
+//! See the [client documentation](https://docs.rs/aws-sdk-sns/latest/aws_sdk_sns_modular/client/struct.Client.html)
 //! for information on what calls can be made, and the inputs and outputs for each of those calls.
 //!
 //! ## Using the SDK
@@ -120,7 +120,7 @@ pub use config::Config;
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let config = aws_config::load_from_env().await;
-/// let client = aws_sdk_sns::Client::new(&config);
+/// let client = aws_sdk_sns_modular::Client::new(&config);
 /// # }
 /// ```
 ///
@@ -132,7 +132,7 @@ pub use config::Config;
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let sdk_config = ::aws_config::load_from_env().await;
-/// let config = aws_sdk_sns::config::Builder::from(&sdk_config)
+/// let config = aws_sdk_sns_modular::config::Builder::from(&sdk_config)
 /// # /*
 ///     .some_service_specific_setting("value")
 /// # */
