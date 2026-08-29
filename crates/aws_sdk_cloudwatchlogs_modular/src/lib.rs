e0796e0fd76ff174056d0b47e1e33fbc586e4198
notmd <notmd1811@gmail.com>
2026-08-29 13:17:54 +0700

    Refresh cloudwatchlogs generated documentation

--- crates/aws_sdk_cloudwatchlogs_modular/src/lib.rs
+++ crates/aws_sdk_cloudwatchlogs_modular/src/lib.rs
@@ -49,12 +49,12 @@
 //! Then in code, a client can be created with the following:
 //!
 //! ```rust,no_run
-//! use aws_sdk_cloudwatchlogs as cloudwatchlogs;
+//! use aws_sdk_cloudwatchlogs_modular as cloudwatchlogs;
 //!
 //! #[::tokio::main]
 //! async fn main() -> Result<(), cloudwatchlogs::Error> {
 //!     let config = aws_config::load_from_env().await;
-//!     let client = aws_sdk_cloudwatchlogs::Client::new(&config);
+//!     let client = aws_sdk_cloudwatchlogs_modular::Client::new(&config);
 //!
 //!     // ... make some calls with the client
 //!
@@ -62,7 +62,7 @@
 //! }
 //! ```
 //!
-//! See the [client documentation](https://docs.rs/aws-sdk-cloudwatchlogs/latest/aws_sdk_cloudwatchlogs/client/struct.Client.html)
+//! See the [client documentation](https://docs.rs/aws-sdk-cloudwatchlogs/latest/aws_sdk_cloudwatchlogs_modular/client/struct.Client.html)
 //! for information on what calls can be made, and the inputs and outputs for each of those calls.
 //!
 //! ## Using the SDK
@@ -123,7 +123,7 @@ pub use config::Config;
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let config = aws_config::load_from_env().await;
-/// let client = aws_sdk_cloudwatchlogs::Client::new(&config);
+/// let client = aws_sdk_cloudwatchlogs_modular::Client::new(&config);
 /// # }
 /// ```
 ///
@@ -135,7 +135,7 @@ pub use config::Config;
 /// ```rust,no_run
 /// # async fn wrapper() {
 /// let sdk_config = ::aws_config::load_from_env().await;
-/// let config = aws_sdk_cloudwatchlogs::config::Builder::from(&sdk_config)
+/// let config = aws_sdk_cloudwatchlogs_modular::config::Builder::from(&sdk_config)
 /// # /*
 ///     .some_service_specific_setting("value")
 /// # */
